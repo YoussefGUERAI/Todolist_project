865e1cd92d96c0bc0f1b1f3de4c3dca0e8bd5a8c
mac <mac@MacBook-Pro-de-mac-7.local>
2025-02-13 18:50:36 +0100

    Add conditions on todo

--- script.js
+++ script.js
@@ -2,20 +2,47 @@ const form = document.querySelector(".add");
 const todolist = document.querySelector(".list-group") ;
 
 form.addEventListener("keydown",function(e){
-    
-    if (e.key === 'Enter'){
-    e.preventDefault();
-    let todo = form.querySelector('input[name="add"]').value ;
-    
-    if (todo != ""){
-    let added = document.createElement("li");
-    added.innerHTML = `<span>${todo}</span>
-    <i class="far fa-trash-alt delete"></i>`;
-    
-    added.classList.add("list-group-item","d-flex", "justify-content-between", "align-items-center");
+    // all needed for adding new items to the list :
 
-    todolist.appendChild(added);
-    };
-}
+    // first checking if the objective is already in the list ; no need to add it once again ;
+    if (e.key === 'Enter'){
+        e.preventDefault();
+        const list = document.querySelectorAll("span");
+        let todo = form.querySelector('input[name="add"]').value ;
+        for (let i =0 ; i<list.length ; i++){
+        
+            if (todo === list[i].innerText){
+                alert("Objective Added already");
+                return;
+            };
+        }
+    // then checking if the objective is non empty ;
+        
+        if (todo != ""){
+        let added = document.createElement("li");
+        added.innerHTML = `<span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>`;
+        
+        added.classList.add("list-group-item","d-flex", "justify-content-between", "align-items-center");
+
+        todolist.appendChild(added);
+        };
+        
+    
+};
+
+
+    // all needed for the search of the objectives ;
+
+    const search = document.querySelector(".search") ;
+    search.addEventListener("keydown",function(e){
+        e.preventDefault();
+        let search_value = search.querySelector('input[name="search"]').value;
+        
+
+
+    });
+
+
 
 });
